48edaee086c9da4678e5bb03cf7babe031e4b7c3
SumierQ <70980581+SumierQ@users.noreply.github.com>
2022-08-30 16:50:08 -0500

    deleted the /create

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -20,7 +20,7 @@ public class StudentController {
 	@Autowired
 	private StudentRepository studentRepository;
 	
-	@PostMapping("/create")
+	@PostMapping("/")
 	public void createStudent(@RequestBody Student student) {
 		studentRepository.insert(student);
 	}
